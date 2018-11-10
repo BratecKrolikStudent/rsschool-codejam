9a8fae3a3e8c9111efb29762c6c6a24428e1aa5b
Dmitriy Kunets <t38@tut.by>
2018-11-10 11:12:01 +0300

    refactor: fix review task # 4

--- src/sumOfOther.js
+++ src/sumOfOther.js
@@ -1,7 +1,4 @@
 module.exports = function sumOfOther(arg) {
-  return arg.map(
-    (value, index, arr) => arr.reduce(
-      (acc, val, idx) => (index === idx ? acc : acc + val), 0,
-    ),
-  );
+  const sumOfArrayElements = arg.reduce((acc, val) => acc + val, 0);
+  return arg.map(value => sumOfArrayElements - value);
 };
